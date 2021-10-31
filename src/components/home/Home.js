e945cbc203c6679deacebbaa58a0dce47aebe4a2
Katarina Lauren <katarina.lauren@gmail.com>
2021-10-31 16:38:18 +0100

    commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,17 @@
 import Heading from "../layout/Heading";
 import Hero from "../layout/Hero";
-
+import heroImage from "../../images/home.jpg";
 
 function Home() {
   return (
     <div>
-      <Hero image={} alt={}>
-      <Heading color={"#000"} fontSize={"3rem"} className={"d-inline"}>
-        Visiting
-      </Heading>
-      <Heading color={"#FFC96C"} fontSize={"3rem"} className={"d-inline ms-2"}>
-        Bergen?
-      </Heading>
+      <Hero image={heroImage} alt={"Bergen"}>
+        <Heading color={"#000"} fontSize={"3rem"} className={"d-inline"}>
+          Visiting
+        </Heading>
+        <Heading color={"#FFC96C"} fontSize={"3rem"} className={"d-inline ms-2"}>
+          Bergen?
+        </Heading>
       </Hero>
     </div>
   );

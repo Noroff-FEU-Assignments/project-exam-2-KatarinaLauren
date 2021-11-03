3509b22c2dbd85f6b3a1c32ec236b4bf7ea1a760
Katarina Lauren <katarina.lauren@gmail.com>
2021-11-03 23:07:26 +0100

    commit

--- src/components/accommodations/AccommodationItem.js
+++ src/components/accommodations/AccommodationItem.js
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
-function AccommodationItem({ id, name }) {
+function AccommodationItem({ id, name, location, rate, image }) {
   return (
     <Link to={`detail/${id}`}>
-      <h5>{name}</h5>
-      <p>{id}</p>
+      <Card className="m-0 mt-3 m-md-3">
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>Location: {location}</Card.Text>
+          <Card.Text>Room rate from: {rate}</Card.Text>
+        </Card.Body>
+      </Card>
     </Link>
   );
 }
@@ -13,6 +20,9 @@ function AccommodationItem({ id, name }) {
 AccommodationItem.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  rate: PropTypes.number.isRequired,
+  image: PropTypes.isRequired,
 };
 
 export default AccommodationItem;
--- src/components/accommodations/RenderAccommodations.js
+++ src/components/accommodations/RenderAccommodations.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { getFromStorage } from "../../utilities/localStorage/localStorageFunctions";
 import AccommodationItem from "./AccommodationItem";
 import Alert from "react-bootstrap/Alert";
+import { BaseUrl } from "../../constants/api";
+
+const url = BaseUrl;
 
 function RenderAccommodations() {
   const [data, setData] = useState([]);
@@ -28,8 +31,10 @@ function RenderAccommodations() {
   return (
     <div className="accommodations">
       {data.map(function (acc) {
-        const { id, name } = acc;
-        return <AccommodationItem key={id} id={id} name={name} />;
+        const imageUrl = acc.images[0].url;
+        const image = url + imageUrl;
+        const { id, name, location, room_rate } = acc;
+        return <AccommodationItem key={id} id={id} name={name} location={location} rate={room_rate} image={image} />;
       })}
     </div>
   );

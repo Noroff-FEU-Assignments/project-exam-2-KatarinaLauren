457fac4f63a2dd90d32446a98b63116b09a7d5d1
Katarina Lauren <katarina.lauren@gmail.com>
2021-11-10 21:25:02 +0100

    Added filter function

--- src/components/accommodations/Accommodations.js
+++ src/components/accommodations/Accommodations.js
@@ -1,9 +1,8 @@
 import PageHeading from "../layout/PageHeading";
-import RenderAccommodations from "./RenderAccommodations";
+import RenderAccommodation from "./RenderAccommodation";
 import Container from "react-bootstrap/Container";
 import Hero from "../layout/Hero";
 import heroImage from "../../images/accommodation/accommodation_hero.jpg";
-import Filters from "./Filters";
 
 function Accommodations() {
   return (
@@ -11,9 +10,8 @@ function Accommodations() {
       <Hero image={heroImage}>
         <PageHeading color={"#fff"}>ACCOMMODATIONS</PageHeading>
       </Hero>
-      <Filters />
       <Container className={"d-flex flex-column justify-content-center"} fluid>
-        <RenderAccommodations />
+        <RenderAccommodation />
       </Container>
     </>
   );
